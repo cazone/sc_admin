f497dc9de3b0b3b674401d4f5f78ede4e1b25e30
Carlos Arenas <cazone@gmail.com.mx>
2022-03-11 15:20:53 -0600

    fix pdf

--- resources/views/pdf/cotizacion.blade.php
+++ resources/views/pdf/cotizacion.blade.php
@@ -105,7 +105,7 @@
 <body>
     <span class="pagenum"></span>
     <div class="invoice-box">
-        <table cellpadding="0" cellspacing="0">
+        <table cellpadding="0" cellspacing="0" style="width:100%" border="1">
             <tr class="top">
                 <td colspan="2">
                     <table>
